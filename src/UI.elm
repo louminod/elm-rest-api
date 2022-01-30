39ebcef118e649c4e2d082bfcfa22815f849ed7e
louminod <30729464+louminod@users.noreply.github.com>
2022-01-30 18:29:07 +0100

    Add files via upload

---
+++ src/UI.elm
@@ -0,0 +1,35 @@
+-- Import the component
+import Ui.Ratings
+
+-- Add it to your model
+type alias Model =
+  { ratings = Ui.Ratings.Model
+  }
+
+-- Add it's messages to your messages
+type Msg
+  = Ratings Ui.Ratings.Msg
+
+-- Add it to your init function
+init : Model
+init =
+  { ratings =
+      Ui.Ratings.init ()
+        |> Ui.Ratings.size 10 -- set things on your component
+  }
+
+-- Update as usual
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg_ model =
+  case msg_ of
+    Ratings msg ->
+      let
+        ( ratings, cmd ) =
+          Ui.Ratings.update msg model.ratings
+      in
+        ( { model | ratings = ratings }, Cmd.map Ratings cmd )
+
+-- Render it in your view
+view : Model -> Html.Html Msg
+view model =
+  Html.map Ratings (Ui.Ratings.view model.ratings)
